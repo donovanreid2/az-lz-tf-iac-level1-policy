934bdcb2a348f150f234951f109f559456662385
donovanreid2 <85472751+donovanreid2@users.noreply.github.com>
2025-10-07 12:46:06 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "subscription_id" {
+  description = "The Azure Subscription ID to be used by the azurerm provider"
+  type        = string
+  default     = "79ce1d43-a7ce-448e-a6fd-6735b57cdb3b"
+}
